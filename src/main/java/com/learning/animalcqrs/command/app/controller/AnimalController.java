638dbf34c9859db9ad1cd2e7d2d614a0ee9f6f3d
mitanderson <mitanderson@deloitte.com.au>
2020-10-18 15:04:50 +1100

    Updated the application.yml to be using correct properly (accidentally used spring data rest property)

--- src/main/java/com/learning/animalcqrs/command/app/controller/AnimalController.java
+++ src/main/java/com/learning/animalcqrs/command/app/controller/AnimalController.java
@@ -4,6 +4,7 @@ import com.learning.animalcqrs.command.domain.Animal;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +14,7 @@ public class AnimalController {
         Animal animal = new Animal("JoJo", "Mammal", "Monkey", 15);
         return ResponseEntity.ok(animal);
     }
+
+
 
 }
