c7365ae932715f38825e32e45d336b4beffb3300
mitanderson <mitanderson@deloitte.com.au>
2020-10-18 14:31:16 +1100

    Spring initializer barebones

---
+++ src/main/java/com/learning/animalcqrs/AnimalCqrsApplication.java
@@ -0,0 +1,13 @@
+package com.learning.animalcqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalCqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalCqrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/animalcqrs/AnimalCqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.animalcqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalCqrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
